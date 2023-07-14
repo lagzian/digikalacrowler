dc5a8ad3d4d4240c3cb7186f1dc148c7ece9df74
lagzian <134182864+lagzian@users.noreply.github.com>
2023-07-14 14:41:44 +0330

    Update script.py

--- script.py
+++ script.py
@@ -1,10 +1,10 @@
 import subprocess
 import sys
-
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'selenium'])
+import time
 
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+
 import os
 import smtplib  
 from email.message import EmailMessage
@@ -12,14 +12,14 @@ import datetime
 from bs4 import BeautifulSoup
 
 options = Options()
-options.headless = True
+options.add_argument('-headless')
 browser = webdriver.Firefox(options=options)
 
 url = 'https://www.digikala.com/incredible-offers/'
-keywords = ['روغن', 'هدفون', 'ماوس']
+keywords = ['روغن', 'هدفون', 'ماوس']  
 
 browser.get(url)
-time.sleep(5)
+time.sleep(5)  
 source = browser.page_source
 soup = BeautifulSoup(source, 'html.parser')
 
@@ -29,10 +29,10 @@ for kw in keywords:
     keyword_found = True
     break
 
-browser.close()  
+browser.close()
 
 test_message = "این یک ایمیل آزمایشی است"
-sender = 'nimahdx2000@gmail.com'  
+sender = 'nimahdx2000@gmail.com'
 recipient = 'tech@zistarvin.ir'
 smtp_server = 'smtp.gmail.com'
 smtp_port = 587
@@ -42,23 +42,24 @@ msg = EmailMessage()
 msg['From'] = sender
 msg['To'] = recipient
 msg['Subject'] = 'Test Email'
-msg.set_content(test_message)
+msg.set_content(test_message) 
 
-smtp = smtplib.SMTP(smtp_server,smtp_port) 
+smtp = smtplib.SMTP(smtp_server,smtp_port)
 smtp.starttls()
-smtp.login(sender, password)  
-smtp.send_message(msg)
+smtp.login(sender, password)
+smtp.send_message(msg) 
 smtp.quit()
 
 if keyword_found:
-  
+
   msg = EmailMessage()
   msg['From'] = sender
   msg['To'] = recipient
   msg['Subject'] = 'Keyword Found'
+  
   content = f'Keywords ({keywords}) found on {url}'
   msg.set_content(content)
-
+  
   smtp.send_message(msg)
 
 print('Done')
